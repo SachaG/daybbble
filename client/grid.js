bba9ec775ffdcae299ab1c2c665281a33ed651d2
Sacha Greif <sacha357@gmail.com>
2014-02-22 16:38:28 +0900

    show hour

--- client/grid.js
+++ client/grid.js
@@ -1,6 +1,10 @@
 Template.grid.helpers({
   days: function () {
-    return Days.find();
+    return Days.find({}, {sort: {timestamp: -1}, limit: 24});
+  },
+  date: function () {
+    var d = new Date(this.timestamp);
+    return d.getHours()+":00";
   },
   shots: function () {
     return jQuery.parseJSON(this.content).shots;
