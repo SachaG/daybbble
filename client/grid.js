ccf847e487836ed45dcd00af3263a9781d387a38
Sacha Greif <sacha357@gmail.com>
2014-02-23 15:46:31 +0900

    added thumbnail preview

--- client/grid.js
+++ client/grid.js
@@ -6,6 +6,12 @@ Template.grid.helpers({
     var d = new Date(this.timestamp);
     return ("0"+d.getHours()).slice(-2)+":"+("0"+d.getMinutes()).slice(-2);
   },
+  likesPerHour: function () {
+    var createdAt= new Date(this.created_at);
+    var now = new Date();
+    var diff = (now.getTime() - createdAt.getTime())/3600000;
+    return Math.round(parseInt(this.likes_count)/diff);
+  },
   shots: function () {
     return jQuery.parseJSON(this.content).shots;
   }
@@ -20,9 +26,18 @@ Template.grid.rendered = function () {
     $(".grid-shot a").addClass('faded');
     $(shotsSelector).removeClass('faded').addClass('highlighted');
 
+    if($(this).offset().left > $(window).width()/2 ){
+      $('.zoom-image').addClass('offset');
+    }else{
+      $('.zoom-image').removeClass('offset')
+    }
+    
+    $('.zoom-image').addClass('visible').attr('src', $(this).attr('data-full-url'))
+
   }, function(){
 
     $('.grid-shot a').removeClass('faded highlighted');
+    $('.zoom-image').removeClass('visible');
 
   });
 }
